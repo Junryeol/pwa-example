00d42159f02e4ecb99b6a786e7775c5d20b05f00
Junryeol <wnsfuf90@gmail.com>
2019-10-28 12:27:41 +0900

    init

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/pwa-example/',
+       '/pwa-example/index.html',
+       '/pwa-example/index.js',
+       '/pwa-example/style.css',
+       '/pwa-example/images/fox1.jpg',
+       '/pwa-example/images/fox2.jpg',
+       '/pwa-example/images/fox3.jpg',
+       '/pwa-example/images/fox4.jpg'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
