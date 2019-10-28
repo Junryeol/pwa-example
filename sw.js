5c88692df42506e7660b04b2f8c057e02e261068
Junryeol <wnsfuf90@gmail.com>
2019-10-28 15:31:47 +0900

    fetch test

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const version = 'v0.1'
-const cachename = 'github-storage'; // 업데이트시 변경
+const cache_version = 'v0.1' // 업데이트시 변경
+const cache_name = 'github-storage';
 
+console.log(self);
 console.log(caches);
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open(cachename+version).then(function(cache) {
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+   caches.open(cache_name+cache_version).then(function(cache) {
      return cache.addAll([
        '/pwa-example/',
        //'/pwa-example/manifest.wmf', // 있으면 업데이트 안됨
@@ -15,19 +16,36 @@ self.addEventListener('install', function(e) {
        '/pwa-example/style.css',
        '/pwa-example/images/fox1.jpg',
        '/pwa-example/images/fox2.jpg',
-       '/pwa-example/images/fox3.jpg',
-       '/pwa-example/images/fox4.jpg'
+       //'/pwa-example/images/fox3.jpg',
+       //'/pwa-example/images/fox4.jpg',
      ]);
    })
  );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log(e);
-  console.log(e.request.url);
-  e.respondWith(
+self.addEventListener('fetch', function(event) {
+  console.log(event);
+  console.log(event.request.url);
+  event.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cache_names) {
+      return Promise.all(
+        cache_names.filter(function(name) {
+          if (cache_name+cache_version == name)
+            return false;
+          else
+            return true;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
     })
   );
 });
